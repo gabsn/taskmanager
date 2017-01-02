8204ad1f813c6ada9f2c3ee724666e0270e91637
Gabin Marignier <gabin.marignier@gmail.com>
2017-01-02 18:10:12 +0100

    Add Context

---
+++ controllers/context.go
@@ -0,0 +1,19 @@
+package controllers
+
+type Context struct {
+    MongoSession *mgo.Session
+}
+
+func (c *Context) Close() {
+    c.MongoSession.Close()
+}
+
+func (c *Context) DbCollection(name string) *mgo.Collection {
+    return c.MongoSession.DB(common.AppConfig.Database).C(name)
+}
+
+func NewContext() *Context {
+    session := common.GetSession().Copy()
+    context := &Context{session}
+    return context
+}
