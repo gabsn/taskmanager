99882e7413609f2d354fe3467ad34ecfe738648e
Gabin Marignier <gabin.marignier@gmail.com>
2017-01-02 17:50:12 +0100

    Add DisplayAppError function to utils.go

--- common/utils.go
+++ common/utils.go
@@ -6,9 +6,19 @@ import (
 	"os"
 )
 
-type config struct {
-	Server, MongoDBHost, DBUser, DBPwd, Database string
-}
+type (
+    config struct {
+        Server, MongoDBHost, DBUser, DBPwd, Database string
+    }
+    appError struct {
+        Error string `json:"error"`
+        Message string `json:"message"`
+        HttpStatus int `json:"status"`
+    }
+    errorResource struct {
+        Data appError `json:"data"`
+    }
+)
 
 var AppConfig config
 
@@ -21,6 +31,16 @@ func initConfig() {
     err = json.NewDecoder(file).Decode(&AppConfig)
     if err != nil {
         log.Fatal("[initConfig]:", err)
+    }
+}
+
+func DisplayAppError(w http.ResponseWriter, handlerError error, message string, code int) {
+    errObj := appError{handlerError.Error(), message, code}
+    log.Printf("[AppError]: %s\n", handlerError)
+    w.Header.Set("Content-Type", "application/json; charset=utf-8")
+    w.WriteHeader(code)
+    if j, err := json.Marshal(errorResource{errObj}); err == nil {
+        w.Write(j)
     }
 }
 
